b3d729cf99361199b53e80e95657d9f9ae74c571
limyixiang <e1121685@u.nus.edu>
2024-05-21 11:54:32 +0800

    Added changes to Kueh Lapis app

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BlockRGB from './components/BlockRGB';
+import { useState } from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+
+  const [colors, setColors] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 255, green: 255, blue: 0, id: "1" },
+    { red: 255, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+  }
+
+  function addColor() {
+    setColors([
+      ...colors,
+      {
+        red: Math.floor(Math.random() * 255),
+        green: Math.floor(Math.random() * 255),
+        blue: Math.floor(Math.random() * 255),
+        id: `${colors.length}`.toString(),
+      }
+    ]);
+  }
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable 
+        style={{ height: 40, justifyContent: "center", width: "100%", alignItems: "center"}}
+        onPress={addColor}>
+        <Text style={{ color: "red", fontWeight: "bold" }}>Add a color</Text>
+      </Pressable>
+      <FlatList style={styles.list} data={colors} renderItem={renderItem} />
     </View>
   );
 }
@@ -14,7 +57,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  list: {
+    width: "100%",
   },
 });
